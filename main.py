52b3a5242a818efceb9d033db7fdeb40c19d2e4f
Gia Huy Ng <giahuynguyen2k@gmail.com>
2021-09-29 23:41:14 +0700

    add CORS and test recieve data from frontend

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex="http://localhost:.*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+class InputData(BaseModel):
+    img: str
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/predict")
+def predict(input: InputData):
+    print(input.img[:50])
+    return {
+        "status": "good",
+    }
