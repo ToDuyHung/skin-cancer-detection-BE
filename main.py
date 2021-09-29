ed54370adc0768286f9a4242e79b4e01df5066d3
Gia Huy Ng <giahuynguyen2k@gmail.com>
2021-09-30 00:26:21 +0700

    update port to 8080

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
@@ -27,3 +28,6 @@ def predict(input: InputData):
     return {
         "status": "good",
     }
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
