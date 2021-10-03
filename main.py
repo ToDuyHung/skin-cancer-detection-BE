cd332a9bdfa986003cac6af0e83a43c58e0ab459
Gia Huy Ng <giahuynguyen2k@gmail.com>
2021-10-03 10:02:37 +0700

    add config file yml

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import uvicorn
+import logging
 
 from MLModel import *
 import utils
 
 app = FastAPI()
+
+config_app = utils.get_config()
+
+logging.basicConfig(filename=config_app['log']['app'],
+                    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+
+model = SimpleANN()
 
 app.add_middleware(
     CORSMiddleware,
@@ -28,8 +36,7 @@ class InputData(BaseModel):
 @app.post("/predict")
 def predict(input: InputData):
     img = utils.base64ToPILImage(input.img)
-    model = SimpleANN()
     return model.predict(img)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host=config_app["server"]["ip_address"], port=int(config_app["server"]["port"]), reload=True)
--- utils.py
+++ utils.py
@@ -1,8 +1,15 @@
 import base64
 from io import BytesIO
 from PIL import Image
+import yaml
 
 def base64ToPILImage(b64str: str):
     im_bytes = base64.b64decode(b64str)
     im_file = BytesIO(im_bytes)
     return Image.open(im_file)
+
+def get_config():
+    with open('app.yml', encoding='utf-8') as cfgFile:
+        config_app = yaml.safe_load(cfgFile)
+
+    return config_app
